c9e82138621462545b0fa9f8289340a03d9c0a60
Christopher <wanjo.christopher@gmail.com>
2024-08-29 22:09:42 -0300

    :books: study: new package metodos

---
+++ Claro_Java/Sintaxe_Java/anatomia-classes/src/dio/christopher/metodos/Metodos.java
@@ -0,0 +1,30 @@
+package dio.christopher.metodos;
+
+public class Metodos {
+
+    // metodo publico as outras classes
+    public void imprimir(String texto){
+    }
+
+    // utiliza-se throw para excecoes que podem ocorrer num metodo
+    public double dividir (int dividendo, int divisor) throws Exception {
+        double resultado = 0;
+        if (divisor == 0){
+            throw new Exception("Can't divide by 0");
+        }
+        return resultado;
+    }
+
+    // metodos private nao podem ser vistos em outras classes
+    private void metodoPrivado(){}
+
+    // comparando metodos
+    public void gravarCliente (String nome, String cpf, int ID){
+        // podemos abstrair esse metodo 
+        // criando uma classe cliente
+        // com todas as caracteristicas
+    }
+    public void gravarCliente(Cliente cliente){}
+    // ou 
+    public void gravar(Cliente cliente){}
+}
